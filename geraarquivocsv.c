be200070479851c1718c4db730deec4c13b9f297
viniciussilvaqueiroz110-beep <viniciussilvaqueiroz110@gmail.com>
2025-12-10 20:25:33 -0300

    Update geraarquivocsv.c

--- geraarquivocsv.c
+++ geraarquivocsv.c
@@ -5,32 +5,18 @@
 #define MAX_SMURFS 3
 #define NAME_LEN 100
 
-int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "help") == 0) {
-        printf("Desenvolvido por Rayan Lima\n");
-        return 0;
-    }
+int main()
 
     char smurfs[MAX_SMURFS][NAME_LEN];
     int capturas[MAX_SMURFS];
     char buffer[128];
 
     for (int i = 0; i < MAX_SMURFS; i++) {
-        printf("Digite o nome do Smurf %d: ", i + 1);
-        if (!fgets(buffer, sizeof(buffer), stdin)) {
-            fprintf(stderr, "Erro ao ler o nome.\n");
-            return 1;
-        }
-        buffer[strcspn(buffer, "\n")] = '\0'; // remove newline
-        strncpy(smurfs[i], buffer, NAME_LEN - 1);
-        smurfs[i][NAME_LEN - 1] = '\0';
+        
+       
+        smurfs[i] = "smurf";
 
-        printf("Digite a quantidade de capturas do Smurf %d: ", i + 1);
-        if (!fgets(buffer, sizeof(buffer), stdin)) {
-            fprintf(stderr, "Erro ao ler as capturas.\n");
-            return 1;
-        }
-        capturas[i] = atoi(buffer);
+        capturas[i] = 1;
     }
 
     FILE *f = fopen("smurfs.csv", "w");
@@ -48,4 +34,5 @@ int main(int argc, char *argv[]) {
     printf("Arquivo smurfs.csv gerado com sucesso!\n");
 
     return 0;
+
 }
